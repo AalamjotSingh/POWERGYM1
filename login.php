6df09c3e7a644a10952b9095a1100584563c0897
Aalamjot Singh <79379106+AalamjotSingh@users.noreply.github.com>
2023-11-22 10:32:17 -0800

    added background image to login

--- login.php
+++ login.php
@@ -4,6 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Login</title>
+    <style>
+    /* Your custom CSS code here */
+    body {
+      background-image: url('final_project_web2\Images\240_F_102528344_XfIuLtPSSBIIR3NYtLRe1QOBUgkdS8Cp.jpg');
+      background-size: cover;
+      background-position: center;
+      background-attachment: fixed;
+      margin: 0;
+    }
+
+    .login-container {
+      background-color: white;
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      margin: 100px auto;
+      padding: 20px;
+      width: 300px;
+    }
+
+    .login-container input[type="text"],
+    .login-container input[type="password"] {
+      width: 100%;
+      padding: 10px;
+      margin: 8px 0;
+      box-sizing: border-box;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      outline: none;
+      transition: border-color 0.3s;
+    }
+
+    .login-container input[type="text"]:focus,
+    .login-container input[type="password"]:focus {
+      border-color: #4CAF50;
+    }
+
+    body::before {
+      content: "";
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: url('your-background-image.jpg');
+      background-size: cover;
+      background-position: center;
+      animation: slide 20s linear infinite;
+      z-index: -1;
+    }
+
+    @keyframes slide {
+      0% {
+        transform: translateX(0%);
+      }
+      100% {
+        transform: translateX(-100%);
+      }
+    }
+  </style>
 </head>
 <body>
     <h2>User Login</h2>
